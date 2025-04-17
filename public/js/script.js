8aabb1d6f3320048f85ffb62e1da99ecb07e83dc
liam woodward <liamwoodward20@outlook.com>
2025-04-17 14:21:23 +0100

    including javascript

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+const userEmail = 'myname@outlook.com';
+const submitBtn = document.getElementById('submit-btn');
+
+submitBtn.addEventListener('click', function(){
+  console.log('you clicked the submit');
+})
